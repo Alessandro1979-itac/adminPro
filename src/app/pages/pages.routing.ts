35747c1af1d524c084f8815021354eff716ef72a
Alessandro1979-itac <sandromuniz27@hotmail.com>
2021-09-24 17:11:15 -0400

    Login e Auth implementados

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AuthGuard } from '../guards/auth.guard';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -13,15 +15,16 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
       { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' } },
       { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Gráfica #1' } },
       { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' } },
       { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
-      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } }
+      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
     ]
-  }
+  },
 ];
 
 @NgModule({
